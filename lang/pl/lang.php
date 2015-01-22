75986b179f2728ee98ddd175f29a5be159662153
Kuba <jakub@c-call.eu>
2015-01-22 16:14:56 +0100

    translations

---
+++ lang/pl/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+'nav' => [
+'serverDebug' => 'Debug Serwera',
+'serverParameters' => 'Parametry',
+'phpInfo' => 'phpInfo'
+],
+
+'permissions' => [
+'access_serverDebug' => 'Dostęp do Debugu Serwera',
+'access_serverParams' => 'Dostęp do Parametrów',
+'access_phpInfo' => 'Dostęp do phpInfo',
+],
+
+'serverparams' => [
+'serverParam' => 'Parametr',
+'serverParamValue' => 'Wartość',
+'key' => 'Klucz',
+'value' => 'Wartość',
+],
+
+];
